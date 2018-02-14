b34a80439bfe192360cb63ce6f7ddd8014241e67
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-02-14 11:43:06 -0500

    added 3rd job definition with less memory intensive job with 3GB, submitted to batch, job is starting, working on adding 4th job def for dockerized-cellranger-python35

--- 2_run_batch_job.py
+++ 2_run_batch_job.py
@@ -16,13 +16,17 @@ for inst_resource in resources:
   resource_type = inst_resource['ResourceType']
   logical_resource_id = inst_resource['LogicalResourceId']
 
-  # 1 single job
+  # job 1
   if logical_resource_id == 'JobDef1':
     job_def_id_1 = inst_resource['PhysicalResourceId'].split('/')[-1].split(':')[0]
 
-  # 2 single job
+  # job 2
   if logical_resource_id == 'JobDef2':
     job_def_id_2 = inst_resource['PhysicalResourceId'].split('/')[-1].split(':')[0]
+
+  # job 3
+  if logical_resource_id == 'JobDef3':
+    job_def_id_3 = inst_resource['PhysicalResourceId'].split('/')[-1].split(':')[0]
 
   if resource_type == 'AWS::Batch::JobQueue':
     job_queue_id = inst_resource['PhysicalResourceId'].split('/')[-1].split(':')[0]
@@ -30,7 +34,7 @@ for inst_resource in resources:
 
 # single-sample-job initialization
 ####################################
-base_name = 'cellranger-2'
+base_name = 'cellranger-dockerhub-nick-3GB'
 params_dict = {}
 params_dict['bucket'] = 'cellranger_bucket'
 
@@ -40,7 +44,7 @@ params_dict['bucket'] = 'cellranger_bucket'
 batch_job_name = base_name # + '-single-1'
 params_dict['inst_fcs'] = 'something'
 # parameters={'inst_argument': json.dumps(params_dict)}
-job_response = client_batch.submit_job(jobDefinition=job_def_id_2,
+job_response = client_batch.submit_job(jobDefinition=job_def_id_3,
                                        jobName=batch_job_name,
                                        jobQueue=job_queue_id)
                                        # parameters=parameters)
