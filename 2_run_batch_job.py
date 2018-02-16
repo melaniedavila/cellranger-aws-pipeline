7e470ada0e3f77584b71059ea9ad77de30f2ce4b
cornhundred <nickfloresfernandez@gmail.com>
2018-02-15 22:44:40 -0500

    trying to submit jobs up to 30GB using the m5.4xlarge instancetype

--- 2_run_batch_job.py
+++ 2_run_batch_job.py
@@ -44,14 +44,14 @@ params_dict['bucket'] = 'cellranger_bucket'
 
 # mounted volume 5GB RAM
 #########################
-batch_job_name = base_name + '-mounted-volume-5GB'
+batch_job_name = base_name + '-5GB'
 job_response = client_batch.submit_job(jobDefinition=job_def_id_1,
                                        jobName=batch_job_name,
                                        jobQueue=job_queue_id)
 
 # mounted volume 10GB
 #######################
-batch_job_name = base_name + '-mounted-volume-10GB'
+batch_job_name = base_name + '-10GB'
 job_response = client_batch.submit_job(jobDefinition=job_def_id_2,
                                        jobName=batch_job_name,
                                        jobQueue=job_queue_id)
@@ -59,14 +59,14 @@ job_response = client_batch.submit_job(jobDefinition=job_def_id_2,
 
 # mounted volume 30GB
 #######################
-batch_job_name = base_name + '-mounted-volume-30GB'
+batch_job_name = base_name + '-30GB'
 job_response = client_batch.submit_job(jobDefinition=job_def_id_3,
                                        jobName=batch_job_name,
                                        jobQueue=job_queue_id)
 
 # # mounted volume 64GB
 # #######################
-# batch_job_name = base_name + '-mounted-volume-64GB'
+# batch_job_name = base_name + '-64GB'
 # job_response = client_batch.submit_job(jobDefinition=job_def_id_4,
 #                                        jobName=batch_job_name,
 #                                        jobQueue=job_queue_id)
