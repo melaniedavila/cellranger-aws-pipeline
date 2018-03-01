c941f31cde19798c46dff4b413e2a677c7cac42f
Manu Garcia <mgarcia.universidad@gmail.com>
2018-03-01 10:11:46 -0500

    Fixed repository ID

--- 1_make_ecr_dockerized_cellranger.py
+++ 1_make_ecr_dockerized_cellranger.py
@@ -2,7 +2,7 @@ import boto3
 client_ecr = boto3.client('ecr')
 
 # single job repo
-response = client_ecr.create_repository(repositoryName='awsbatch/dockerized-cellranger')
+response = client_ecr.create_repository(repositoryName='awsbatch/cellranger-aws-pipeline')
 print(response['repository']['repositoryUri'])
 
 
