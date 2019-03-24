fc8fd35441631bb27ede3cc6abd35e0ebb210272
Melanie Davila <davilam91@gmail.com>
2019-03-23 20:13:13 -0400

    Remove archived script

--- archive/1_make_ecr_dockerized_cellranger.py
+++
@@ -1,9 +0,0 @@
-import boto3
-client_ecr = boto3.client('ecr')
-
-# single job repo
-response = client_ecr.create_repository(repositoryName='awsbatch/cellranger-aws-pipeline')
-print(response['repository']['repositoryUri'])
-
-
-# 519400500372.dkr.ecr.us-east-1.amazonaws.com/awsbatch/dockerized-cellranger
