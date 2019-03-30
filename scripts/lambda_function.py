2082520c4e4477e279799bcc0be53fa4a9f8a832
Melanie Davila <davilam91@gmail.com>
2019-03-30 17:29:58 -0400

    Finish the script that submits batch jobs

--- scripts/lambda_function.py
+++ scripts/lambda_function.py
@@ -1,35 +1,43 @@
+#!/usr/bin/env python3.6
+
 import boto3
 import datetime as dt
 import json
+import sys
 
 AWS_ECR_REGISTRY = '402084680610.dkr.ecr.us-east-1.amazonaws.com'
 
 PIPELINE_NAME = 'test-cellranger-pipeline'
-JOB_QUEUE = PIPELINE_NAME # TODO: verify that this name is correct
-ANALYSIS_JOB_NAME = f'{PIPELINE_NAME}-analysis'
-MKFASTQ_JOB_NAME = f'{PIPELINE_NAME}-mkfastq'
+JOB_QUEUE = 'test-10xpipeline'
+JOB_NAME = f'{PIPELINE_NAME}-mkfastq' # TODO: remove mkfastq from job name
 
 batch_client = boto3.client('batch')
 
-def experiment_name(run, himc_pool, sequencing_date):
+def generate_experiment_name(run, himc_pool, sequencing_date, **_):
     sequencing_date_object = dt.datetime.strptime(sequencing_date, "%Y-%m-%d").date()
-    return f'run{run}-himc{himc_pool}-{sequencing_date_object.strftime(""%m%d%y")}'
+    return f'run{run}-himc{himc_pool}-{sequencing_date_object.strftime("%m%d%y")}'
 
-def submit_analysis(sample, experiment, image_name, depends_on = []):
+def submit_analysis(sample, experiment, depends_on = []):
+    experiment_name = generate_experiment_name(**experiment)
     job_configuration = {
-        "experiment_name": experiment_name(experiment),
+        "experiment_name": experiment_name,
         "sample": sample
     }
     parameters = {
-        "image_name": image_name
+        "command": "analysis",
         "configuration": json.dumps(job_configuration)
     }
 
+    print()
+    print()
+    print(depends_on)
+    print()
+    print()
     try:
         response = batch_client.submit_job(
             dependsOn=depends_on,
-            jobDefinition=job_definition,
-            jobName=ANALYSIS_JOB_NAME,
+            jobDefinition=f'{JOB_NAME}',
+            jobName=f'{JOB_NAME}-{experiment_name}-{sample["job_type"]}-{sample["job_name"]}',
             jobQueue=JOB_QUEUE,
             parameters=parameters
         )
@@ -42,21 +50,22 @@ def submit_analysis(sample, experiment, image_name, depends_on = []):
         print(message)
         raise Exception(message)
 
-def submit_mkfastq(samples, experiment, image_name):
+def submit_mkfastq(samples, experiment):
+    experiment_name = generate_experiment_name(**experiment)
     job_configuration = {
-        "experiment_name": experiment_name(experiment),
-        "run_id": experiment["run_id"],
+        "experiment_name": experiment_name,
+        "run": experiment["run"],
         "samples": samples
     }
     parameters = {
-        "image_name": image_name,
+        "command": "mkfastq",
         "configuration": json.dumps(job_configuration)
     }
 
     try:
         response = batch_client.submit_job(
-            jobDefinition=job_definition,
-            jobName=ANALYSIS_JOB_NAME,
+            jobDefinition=f'{JOB_NAME}',
+            jobName=f'{JOB_NAME}-{experiment_name}-mkfastq',
             jobQueue=JOB_QUEUE,
             parameters=parameters
         )
@@ -71,29 +80,27 @@ def submit_mkfastq(samples, experiment, image_name):
 
 def lambda_handler(event, context):
     configuration = event['configuration']
-
-    bcl2fastq_version = configuration['experiment']['bcl2fastq_version']
-    cellranger_version = configuration['experiment']['cellranger_version']
-    image_name = f'{AWS_ECR_REGISTRY}/cellranger-{cellranger_version}-bcl2fastq-{bcl2fastq_version}'
+    experiment = configuration['experiment']
+    bcl2fastq_version = experiment['bcl2fastq_version']
+    cellranger_version = experiment['cellranger_version']
 
     processing = configuration['processing']
     processing_job_ids = []
 
     if processing['mkfastq']:
-        print(f'info: processing: mkfastq: submitting: {sample["name"]}')
+        print(f'info: processing: mkfastq: submitting')
         mkfastq_job_id = submit_mkfastq(processing['mkfastq'],
-                                        experiment=experiment,
-                                        image_name=image_name)
+                                        experiment=experiment)
         processing_job_ids.append(mkfastq_job_id)
-        print(f'info: processing: mkfastq: submitted: {sample["name"]}')
+        print(f'info: processing: mkfastq: submitted')
 
     if configuration['analyses']:
         for sample in configuration['analyses']['samples']:
             print(f'info: analyses: submitting: {sample["name"]}')
-            submit_analysis(sample,
-                            experiment=experiment,
-                            image_name=image_name,
-                            depends_on=processing_job_ids)
+            # submit_analysis(sample,
+            #                 experiment=experiment,
+            #                 depends_on= [ {"jobId": job_id} for job_id in processing_job_ids ])
+            print('FIXME: PRETENDING TO SUBMIT')
             print(f'info: analyses: submitted: {sample["name"]}')
 
     print("info: all jobs submitted successfully.")
@@ -102,3 +109,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': {}
     }
+
+if __name__ == "__main__":
+    event = json.load(sys.stdin)
+    lambda_handler(event, None)
