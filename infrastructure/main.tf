01b888b6d529608ac1a572a86cd438cd8ca086a5
Melanie Davila <davilam91@gmail.com>
2019-06-22 18:23:57 -0400

    Rename main terraform file

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region  = "us-east-1"
   version = "~> 2.15"
-
 }
 
 # BEGIN COMPUTE ENVIRONMENT RESOURCES
@@ -70,8 +69,8 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.this.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
@@ -89,7 +88,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.public.id
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
 
