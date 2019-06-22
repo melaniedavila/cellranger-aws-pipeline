8c742315861418bea56c3ff1798085c1a8d5b7e2
Melanie Davila <davilam91@gmail.com>
2019-06-22 13:48:25 -0400

    Rename many things

--- infrastructure/test-cellranger-pipeline.tf
+++ infrastructure/test-cellranger-pipeline.tf
@@ -59,26 +59,26 @@ resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
-resource "aws_vpc" "test-cellranger-pipeline" {
+resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "test-cellranger-pipeline"
+    Name = "cellranger-pipeline"
   }
 }
 
-resource "aws_subnet" "main" {
-  vpc_id = "${aws_vpc.test-cellranger-pipeline.id}"
+resource "aws_subnet" "private" {
+  vpc_id = "${aws_vpc.this.id}"
   cidr_block = "10.0.0.0/24"
 
   tags = {
-    Name = "main"
+    Name = "private"
   }
 }
 
-resource "aws_security_group" "test-cellranger-pipeline" {
+resource "aws_security_group" "all_outbound" {
   name = "test-cellranger-pipeline"
-  vpc_id = "${aws_vpc.test-cellranger-pipeline.id}"
+  vpc_id = "${aws_vpc.this.id}"
 }
 
 resource "aws_security_group_rule" "allow_all_outbound" {
@@ -91,10 +91,10 @@ resource "aws_security_group_rule" "allow_all_outbound" {
   protocol        = "all"
   cidr_blocks     = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.test-cellranger-pipeline.id
+  security_group_id = aws_security_group.all_outbound.id
 }
 
-resource "aws_batch_compute_environment" "test-cellranger-pipeline" {
+resource "aws_batch_compute_environment" "cellranger_pipeline" {
   compute_environment_name = "test-cellranger-pipeline"
   type = "MANAGED"
 
@@ -107,9 +107,9 @@ resource "aws_batch_compute_environment" "test-cellranger-pipeline" {
     max_vcpus = 256
     min_vcpus = 0
 
-    security_group_ids = [aws_security_group.test-cellranger-pipeline.id]
+    security_group_ids = [aws_security_group.all_outbound.id]
 
-    subnets = ["${aws_subnet.main.id}"]
+    subnets = [aws_subnet.private.id]
 
     tags = {
       # TODO: don't repeat this name, we already defined above
@@ -125,15 +125,15 @@ resource "aws_batch_compute_environment" "test-cellranger-pipeline" {
 
 # END COMPUTE ENVIRONMENT RESOURCES
 
-resource "aws_batch_job_queue" "test-cellranger-pipeline" {
+resource "aws_batch_job_queue" "this" {
   name = "test-cellranger-pipeline"
   state                = "ENABLED"
   priority             = 10
-  compute_environments = ["${aws_batch_compute_environment.test-cellranger-pipeline.arn}"]
+  compute_environments = [aws_batch_compute_environment.cellranger_pipeline.arn]
 }
 
 # TODO: DRY up the job definitions
-resource "aws_batch_job_definition" "test-cellranger-pipeline-cellranger-2_2_0-bcl2fastq-2_20_0" {
+resource "aws_batch_job_definition" "cellranger_2_2_0_bcl2fastq_2_20_0" {
   name = "test-cellranger-pipeline-cellranger-2_2_0-bcl2fastq-2_20_0"
   type = "container"
 
@@ -180,7 +180,7 @@ resource "aws_batch_job_definition" "test-cellranger-pipeline-cellranger-2_2_0-b
 CONTAINER_PROPERTIES
 }
 
-resource "aws_batch_job_definition" "test-cellranger-pipeline-cellranger-3_0_2-bcl2fastq-2_20_0" {
+resource "aws_batch_job_definition" "cellranger_3_0_2_bcl2fastq_2_20_0" {
   name = "test-cellranger-pipeline-cellranger-3_0_2-bcl2fastq-2_20_0"
   type = "container"
 
@@ -228,10 +228,10 @@ CONTAINER_PROPERTIES
 }
 
 # TODO: dry this up
-resource "aws_ecr_repository" "cellranger-2_2_0-bcl2fastq-2_20_0" {
+resource "aws_ecr_repository" "cellranger_2_2_0_bcl2fastq_2_20_0" {
   name = "cellranger-2.2.0-bcl2fastq-2.20.0"
 }
 
-resource "aws_ecr_repository" "cellranger-3_0_2-bcl2fastq-2_20_0" {
+resource "aws_ecr_repository" "cellranger_3_0_2_bcl2fastq_2_20_0" {
   name = "cellranger-3.0.2-bcl2fastq-2.20.0"
 }
