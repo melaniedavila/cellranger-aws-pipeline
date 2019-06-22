78da338b5465a9356a25431326bf7a52927664c6
Melanie Davila <davilam91@gmail.com>
2019-06-22 18:36:16 -0400

    Define cellranger-bcl2fastq version combinations

--- infrastructure/vars.tf
+++ infrastructure/vars.tf
@@ -1,1 +1,14 @@
 variable "environment" {}
+variable "cellranger_bcl2fastq_version_pairs" {
+  # Don't change this default
+  default = [
+    {
+      cellranger_version = "2.2.0",
+      bcl2fastq_version = "2.20.0"
+    },
+    {
+      cellranger_version = "3.0.2",
+      bcl2fastq_version = "2.20.0"
+    }
+  ]
+}
