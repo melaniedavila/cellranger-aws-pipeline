8873e85324485c34ba2524feeaa78f0f287524f1
Melanie Davila <davilam91@gmail.com>
2019-06-22 20:46:40 -0400

    Stop using JSON heredocs
    also, format again

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,24 +8,24 @@ provider "aws" {
 resource "aws_iam_role" "ecs_instance_role" {
   name = "ecsInstanceRole"
 
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
+  assume_role_policy = jsonencode(
     {
-        "Action": "sts:AssumeRole",
-        "Effect": "Allow",
-        "Principal": {
-        "Service": "ec2.amazonaws.com"
-        }
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "ec2.amazonaws.com"
+          }
+        },
+      ]
+      Version = "2012-10-17"
     }
-    ]
-}
-EOF
+  )
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
-  role = aws_iam_role.ecs_instance_role.name
+  role       = aws_iam_role.ecs_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
@@ -35,7 +35,7 @@ resource "aws_iam_instance_profile" "ecs_instance_role" {
 }
 
 resource "aws_iam_role" "pipeline" {
-  name = "cellranger-pipeline"
+  name        = "cellranger-pipeline"
   description = "Allow ECS containers to read input and write output to S3."
   assume_role_policy = jsonencode(
     {
@@ -54,8 +54,8 @@ resource "aws_iam_role" "pipeline" {
 }
 
 resource "aws_iam_role" "aws_batch_service_role" {
-  name = "AWSBatchServiceRole"
-  path = "/service-role/"
+  name               = "AWSBatchServiceRole"
+  path               = "/service-role/"
   assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -73,7 +73,7 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
-  role       = aws_iam_role.aws_batch_service_role.name
+  role = aws_iam_role.aws_batch_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
@@ -88,8 +88,8 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.1.0/24"
+  vpc_id = aws_vpc.this.id
+  cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
@@ -107,7 +107,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+  subnet_id = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -116,7 +116,7 @@ resource "aws_internet_gateway" "this" {
 }
 
 resource "aws_security_group" "all_outbound" {
-  name   = "all-inbound"
+  name = "all-inbound"
   vpc_id = aws_vpc.this.id
 }
 
@@ -124,10 +124,10 @@ resource "aws_security_group_rule" "allow_all_outbound" {
   # this rule is necessary because batch won't be able to launch
   # containers without it. see
   # https://aws.amazon.com/premiumsupport/knowledge-center/batch-job-stuck-runnable-status/
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "all"
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "all"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.all_outbound.id
@@ -137,7 +137,7 @@ resource "aws_security_group_rule" "allow_all_outbound" {
 
 resource "aws_batch_compute_environment" "cellranger_pipeline" {
   compute_environment_name = "${var.environment}-cellranger-pipeline"
-  type                     = "MANAGED"
+  type = "MANAGED"
 
   compute_resources {
     image_id = "ami-000f5114abc141b76"
@@ -160,15 +160,15 @@ resource "aws_batch_compute_environment" "cellranger_pipeline" {
   }
 
   service_role = aws_iam_role.aws_batch_service_role.arn
-  depends_on   = ["aws_iam_role_policy_attachment.aws_batch_service_role"]
+  depends_on = ["aws_iam_role_policy_attachment.aws_batch_service_role"]
 }
 
 # END COMPUTE ENVIRONMENT RESOURCES
 
 resource "aws_batch_job_queue" "this" {
-  name                 = "${var.environment}-cellranger-pipeline"
-  state                = "ENABLED"
-  priority             = 10
+  name = "${var.environment}-cellranger-pipeline"
+  state = "ENABLED"
+  priority = 10
   compute_environments = [aws_batch_compute_environment.cellranger_pipeline.arn]
 }
 
@@ -193,39 +193,39 @@ resource "aws_batch_job_definition" "main" {
     attempt_duration_seconds = 129600
   }
 
-  container_properties = <<CONTAINER_PROPERTIES
-{
-  "image": "402084680610.dkr.ecr.us-east-1.amazonaws.com/cellranger-${element(var.cellranger_bcl2fastq_version_pairs, count.index)["cellranger_version"]}-bcl2fastq-${element(var.cellranger_bcl2fastq_version_pairs, count.index)["bcl2fastq_version"]}",
-  "vcpus": 16,
-  "memory": 126976,
-  "command": [
-    "Ref::command",
-    "Ref::configuration"
-  ],
-  "jobRoleArn": "arn:aws:iam::402084680610:role/cellranger-pipeline",
-  "volumes": [
+  container_properties = jsonencode(
     {
-      "host": {
-        "sourcePath": "/docker_scratch"
-      },
-      "name": "scratch"
+      command = [
+        "Ref::command",
+        "Ref::configuration",
+      ]
+      environment = [
+        {
+          name = "DEBUG"
+          value = "false"
+        },
+      ]
+      image = "402084680610.dkr.ecr.us-east-1.amazonaws.com/cellranger-${element(var.cellranger_bcl2fastq_version_pairs, count.index)["cellranger_version"]}-bcl2fastq-${element(var.cellranger_bcl2fastq_version_pairs, count.index)["bcl2fastq_version"]}"
+      jobRoleArn = aws_iam_role.pipeline.arn
+      memory = 126976
+      mountPoints = [
+        {
+          containerPath = "/home/cellranger/scratch"
+          sourceVolume = "scratch"
+        },
+      ]
+      ulimits = []
+      vcpus = 16
+      volumes = [
+        {
+          host = {
+            sourcePath = "/docker_scratch"
+          }
+          name = "scratch"
+        },
+      ]
     }
-  ],
-  "environment": [
-    {
-      "name": "DEBUG",
-      "value": "false"
-    }
-  ],
-  "mountPoints": [
-    {
-      "containerPath": "/home/cellranger/scratch",
-      "sourceVolume": "scratch"
-    }
-  ],
-  "ulimits": []
-}
-CONTAINER_PROPERTIES
+  )
 }
 
 resource "aws_ecr_repository" "main" {
