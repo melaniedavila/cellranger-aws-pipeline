791f6519090a78f3f9453b3fb7072539c07bc01e
Melanie Davila <davilam91@gmail.com>
2019-06-23 16:53:14 -0400

    Move VPC resource to a separate file

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -77,64 +77,6 @@ resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
-# BEGIN VPC
-
-resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "cellranger-pipeline"
-  }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.this.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-}
-
-resource "aws_security_group" "all_outbound" {
-  name = "all-inbound"
-  vpc_id = aws_vpc.this.id
-}
-
-resource "aws_security_group_rule" "allow_all_outbound" {
-  # this rule is necessary because batch won't be able to launch
-  # containers without it. see
-  # https://aws.amazon.com/premiumsupport/knowledge-center/batch-job-stuck-runnable-status/
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "all"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.all_outbound.id
-}
-
-# END VPC
-
 resource "aws_batch_compute_environment" "cellranger_pipeline" {
   compute_environment_name = "${var.environment}-cellranger-pipeline"
   type = "MANAGED"
---
+++ infrastructure/vpc.tf
@@ -0,0 +1,63 @@
+# Don't edit this file unless you know what you're doing.
+
+# This file defines the networking resources that the cellranger
+# pipeline depends on; the `cellranger-pipeline` VPC is the largest of
+# these resources, and it contains everything else defined in this
+# file.
+
+# These resources are foundational to the pipeline; we build upon
+# them, and they are unlikely to be changed.
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "cellranger-pipeline"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.this.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_security_group" "all_outbound" {
+  name = "all-inbound"
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  # this rule is necessary because batch won't be able to launch
+  # containers without it. see
+  # https://aws.amazon.com/premiumsupport/knowledge-center/batch-job-stuck-runnable-status/
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "all"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.all_outbound.id
+}
